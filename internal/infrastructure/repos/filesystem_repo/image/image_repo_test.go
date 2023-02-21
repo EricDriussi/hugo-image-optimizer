155dfeceb72659368f512eff3d2d2486423f2a74
Eric Driussi <ericdriussi@gmail.com>
2023-02-21 15:38:04 +0000

    add delete function to image repo

--- internal/infrastructure/repos/filesystem_repo/image/image_repo_test.go
+++ internal/infrastructure/repos/filesystem_repo/image/image_repo_test.go
@@ -1,45 +1,89 @@
 package filesystemrepo_test
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"runtime"
 	"testing"
 
+	"github.com/EricDriussi/hugo-image-optimizer/internal/domain"
 	filesystemrepo "github.com/EricDriussi/hugo-image-optimizer/internal/infrastructure/repos/filesystem_repo/image"
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_ImageRepository_Load(t *testing.T) {
+func Test_ImageRepository(t *testing.T) {
 	setCWDToProjectRoot()
-	validDir := "test/data/images/"
+	images_test_dir := "test/data/images/"
 
-	t.Run("Loads all images recursively if directory exists", func(t *testing.T) {
-		repo := filesystemrepo.NewImage(validDir, []string{})
+	t.Run("#LOAD", func(t *testing.T) {
+		t.Run("Loads all images recursively if directory exists", func(t *testing.T) {
+			repo := filesystemrepo.NewImage(images_test_dir, []string{})
 
-		loadedImages, err := repo.Load()
+			loadedImages, err := repo.Load()
 
-		assert.Len(t, loadedImages, 5)
-		assert.NoError(t, err)
+			assert.Len(t, loadedImages, 5)
+			assert.NoError(t, err)
+		})
+
+		t.Run("Doesn't load images from excluded directories", func(t *testing.T) {
+			excludedDirs := []string{"whoami", "donation"}
+			repo := filesystemrepo.NewImage(images_test_dir, excludedDirs)
+
+			loadedImages, err := repo.Load()
+
+			assert.Len(t, loadedImages, 3)
+			assert.NoError(t, err)
+		})
+
+		t.Run("Errors out if given a non existent directory", func(t *testing.T) {
+			repo := filesystemrepo.NewImage("test/data/images/NON_EXISTENT/", []string{})
+
+			loadedImages, err := repo.Load()
+
+			assert.Len(t, loadedImages, 0)
+			assert.Error(t, err)
+		})
 	})
 
-	t.Run("Doesn't load images from excluded directories", func(t *testing.T) {
-		excludedDirs := []string{"whoami", "donation"}
-		repo := filesystemrepo.NewImage(validDir, excludedDirs)
+	t.Run("#DELETE", func(t *testing.T) {
+		t.Run("Doesn't delete images from excluded directories", func(t *testing.T) {
+			excludedDirs := []string{"whoami", "donation"}
+			repo := filesystemrepo.NewImage(images_test_dir, excludedDirs)
 
-		loadedImages, err := repo.Load()
+			filename := fmt.Sprintf("%s%s", excludedDirs[0], "testFile.png")
+			image, image_err := domain.NewImage(filename)
+			assert.NoError(t, image_err)
 
-		assert.Len(t, loadedImages, 3)
-		assert.NoError(t, err)
-	})
+			f, create_err := os.Create(filename)
+			defer f.Close()
+			assert.NoError(t, create_err)
 
-	t.Run("Errors out if given a non existent directory", func(t *testing.T) {
-		repo := filesystemrepo.NewImage("test/data/images/NON_EXISTENT/", []string{})
+			repo_err := repo.Delete(image)
+			assert.NoError(t, repo_err)
 
-		loadedImages, err := repo.Load()
+			rm_err := os.Remove(filename)
+			assert.NoError(t, rm_err)
+		})
 
-		assert.Len(t, loadedImages, 0)
-		assert.Error(t, err)
+		t.Run("Deletes an image", func(t *testing.T) {
+			excludedDirs := []string{"whoami", "donation"}
+			repo := filesystemrepo.NewImage(images_test_dir, excludedDirs)
+
+			filename := fmt.Sprintf("%s%s", images_test_dir, "testFile.png")
+			image, image_err := domain.NewImage(filename)
+			assert.NoError(t, image_err)
+
+			f, create_err := os.Create(filename)
+			defer f.Close()
+			assert.NoError(t, create_err)
+
+			repo_err := repo.Delete(image)
+			assert.NoError(t, repo_err)
+
+			rm_err := os.Remove(filename)
+			assert.Error(t, rm_err)
+		})
 	})
 }
 
--- internal/infrastructure/repos/filesystem_repo/image/main.go
+++ internal/infrastructure/repos/filesystem_repo/image/main.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/EricDriussi/hugo-image-optimizer/internal/domain"
 )
 
 type fsrepo struct {
@@ -16,6 +18,25 @@ func NewImage(images_dir string, excluded []string) fsrepo {
 		images_dir:    images_dir,
 		excluded_dirs: excluded,
 	}
+}
+
+func (r fsrepo) Delete(image domain.Image) error {
+	return filepath.Walk(r.images_dir, func(path string, file os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if file.IsDir() {
+			if r.isInExcludedList(file.Name()) {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+		if strings.Contains(path, image.GetPath()) {
+			return os.Remove(image.GetPath())
+		}
+		return nil
+	})
 }
 
 func (r fsrepo) Load() ([]string, error) {
@@ -37,7 +58,7 @@ func (r fsrepo) loadImagesInto(images_list *[]string) filepath.WalkFunc {
 			return nil
 		}
 
-		*images_list = append(*images_list, file.Name())
+		*images_list = append(*images_list, path)
 		return nil
 	}
 }
