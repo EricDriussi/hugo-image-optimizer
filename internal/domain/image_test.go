c08fbcdcf1660fdad0997cbf314a3fd34cea3522
Eric Driussi <ericdriussi@gmail.com>
2023-02-21 13:24:35 +0000

    refactor image tests

--- internal/domain/image_test.go
+++ internal/domain/image_test.go
@@ -1,7 +1,7 @@
 package domain_test
 
 import (
-	"strings"
+	"fmt"
 	"testing"
 
 	"github.com/EricDriussi/hugo-image-optimizer/internal/domain"
@@ -10,16 +10,18 @@ import (
 
 func Test_Image(t *testing.T) {
 	t.Run("errors out if given an invalid file", func(t *testing.T) {
-		filepath := "path/with/noExtension"
-		image, err := domain.NewImage(filepath)
+		invalid_filepaths := []string{"path/with/noExtension", ""}
+		for _, filepath := range invalid_filepaths {
+			image, err := domain.NewImage(filepath)
 
-		assert.Error(t, err)
-		assert.Equal(t, domain.Image{}, image)
+			assert.Error(t, err)
+			assert.Equal(t, domain.Image{}, image)
+		}
 	})
 
 	t.Run("builds as expected with no errors", func(t *testing.T) {
 		extension := ".png"
-		filepath := strings.Join([]string{"path/with/validExtension", extension}, "")
+		filepath := fmt.Sprintf("path/with/validExtension%s", extension)
 		image, err := domain.NewImage(filepath)
 
 		assert.NoError(t, err)
@@ -29,14 +31,19 @@ func Test_Image(t *testing.T) {
 
 	t.Run("checks if is present in reference list", func(t *testing.T) {
 		filename := "valid.png"
-		image_path := strings.Join([]string{"path/with/", filename}, "")
+		filepath := fmt.Sprintf("/a/long/path/to/%s", filename)
 
-		image_ref1 := strings.Join([]string{"/with/", filename}, "")
-
-		image, err := domain.NewImage(image_path)
+		image, err := domain.NewImage(filepath)
 		assert.NoError(t, err)
 
-		present := image.IsPresentIn([]string{image_ref1})
+		matching_reference := fmt.Sprintf("/path/to/%s", filename)
+		non_matching_reference := fmt.Sprintf("/irrelevant/path/%s", filename)
+		image_references := []string{matching_reference, non_matching_reference}
+
+		present := image.IsPresentIn(image_references)
 		assert.True(t, present)
+
+		non_present := !image.IsPresentIn([]string{non_matching_reference})
+		assert.True(t, non_present)
 	})
 }
