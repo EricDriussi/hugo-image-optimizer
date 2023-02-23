a1172ed3a8a577edf13db72a2a399a42fccbacbc
Eric Driussi <ericdriussi@gmail.com>
2023-02-23 08:16:19 +0000

    refactor and add to image serv test

--- internal/services/image/image_service_test.go
+++ internal/services/image/image_service_test.go
@@ -14,21 +14,37 @@ func Test_ImageService(t *testing.T) {
 	pathOne := "a/file/path/image.png"
 	pathTwo := "a/file/path/to/another/image.jpg"
 	images := []string{pathOne, pathTwo}
+	anError := errors.New("Something went wrong")
 
-	t.Run("Deletes only unreferenced images", func(t *testing.T) {
+	t.Run("Deletes", func(t *testing.T) {
 		image, image_err := domain.NewImage(pathTwo)
 		assert.NoError(t, image_err)
-
-		imageRepositoryMock := new(mocks.ImageRepository)
-		imageRepositoryMock.On("Load").Return(images, nil)
-		imageRepositoryMock.On("Delete", image).Return(nil)
-
-		imageService := services.NewImage(imageRepositoryMock)
 		image_reference := "/path/image.png"
-		service_err := imageService.RemoveAllExcept([]string{image_reference})
-		imageRepositoryMock.AssertExpectations(t)
 
-		assert.NoError(t, service_err)
+		t.Run("only unreferenced images", func(t *testing.T) {
+			imageRepositoryMock := new(mocks.ImageRepository)
+			imageRepositoryMock.On("Load").Return(images, nil)
+			imageRepositoryMock.On("Delete", image).Return(nil)
+
+			imageService := services.NewImage(imageRepositoryMock)
+			service_err := imageService.RemoveAllExcept([]string{image_reference})
+			imageRepositoryMock.AssertExpectations(t)
+
+			assert.NoError(t, service_err)
+		})
+
+		t.Run("stopping if the repository erros out", func(t *testing.T) {
+			imageRepositoryMock := new(mocks.ImageRepository)
+			imageRepositoryMock.On("Load").Return(images, nil)
+			imageRepositoryMock.On("Delete", image).Return(anError)
+
+			imageService := services.NewImage(imageRepositoryMock)
+			err := imageService.RemoveAllExcept([]string{image_reference})
+			imageRepositoryMock.AssertExpectations(t)
+
+			assert.Error(t, err)
+			assert.ErrorContains(t, err, "Failed to delete images")
+		})
 	})
 
 	t.Run("Loads", func(t *testing.T) {
@@ -59,7 +75,6 @@ func Test_ImageService(t *testing.T) {
 		})
 
 		t.Run("stopping if the repository erros out", func(t *testing.T) {
-			anError := errors.New("Something went wrong")
 			imageRepositoryMock := new(mocks.ImageRepository)
 			imageRepositoryMock.On("Load").Return(nil, anError)
 
@@ -70,6 +85,41 @@ func Test_ImageService(t *testing.T) {
 			assert.Nil(t, loadedImages)
 			assert.Error(t, err)
 			assert.ErrorContains(t, err, "Failed to load images")
+		})
+	})
+
+	t.Run("Converts", func(t *testing.T) {
+		images := []string{pathOne, pathTwo}
+		image1, image1_err := domain.NewImage(pathOne)
+		assert.NoError(t, image1_err)
+		image2, image2_err := domain.NewImage(pathTwo)
+		assert.NoError(t, image2_err)
+
+		t.Run("all images", func(t *testing.T) {
+			imageRepositoryMock := new(mocks.ImageRepository)
+			imageRepositoryMock.On("Load").Return(images, nil)
+			imageRepositoryMock.On("ConvertToWebp", image1).Return(nil)
+			imageRepositoryMock.On("ConvertToWebp", image2).Return(nil)
+
+			imageService := services.NewImage(imageRepositoryMock)
+			err := imageService.Convert()
+
+			imageRepositoryMock.AssertExpectations(t)
+			assert.NoError(t, err)
+		})
+
+		t.Run("stopping if the repository erros out", func(t *testing.T) {
+			imageRepositoryMock := new(mocks.ImageRepository)
+			imageRepositoryMock.On("Load").Return(images, nil)
+			imageRepositoryMock.On("ConvertToWebp", image1).Return(nil)
+			imageRepositoryMock.On("ConvertToWebp", image2).Return(anError)
+
+			imageService := services.NewImage(imageRepositoryMock)
+			err := imageService.Convert()
+			imageRepositoryMock.AssertExpectations(t)
+
+			assert.Error(t, err)
+			assert.ErrorContains(t, err, "Failed to convert images")
 		})
 	})
 }
